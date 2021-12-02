e959e8cad13cc2baa13375c03324cd11ada6fe1d
MilesRay <Trexcontroller@gmail.com>
2021-12-02 13:08:52 +0000

    day2

---
+++ day2.py
@@ -0,0 +1,14 @@
+f = open("day2.txt")
+
+file_contents = f.read()
+
+file_contents = file_contents.split(" ")
+
+new_list = [int(n) for n in file_contents]
+
+for i in range(1,len(new_list)):
+    print(new_list[i])
+
+
+
+
