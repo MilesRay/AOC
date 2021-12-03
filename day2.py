b047a0564a6a3461cceee14c3882bf8c107b52cd
MilesRay <89792182+MilesRay@users.noreply.github.com>
2021-12-03 00:09:33 +0000

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,13 +1,35 @@
-f = open("Github/AOC/day2.txt")
+f = open("day2.txt")
 
 file_contents = f.read()
 
 file_contents = file_contents.split(" ")
-new_list = [int(n) for n in file_contents]
+new_list = [n for n in file_contents]
 
 for i in range(0, len(new_list)):
     print(new_list[i])
 
+
+f = open("day2.txt")
+
+file_contents = f.read()
+
+forwardCount = 0
+depthCount = 0
+file_contents = file_contents.split(" ")
+new_list = [n for n in file_contents]
+
+for i in range(0, len(new_list)):
+    if new_list[i] == "forward":
+        forwardCount = forwardCount + int(new_list[i+1][:1])
+
+    if new_list[i] == "up":
+        depthCount = depthCount + int(new_list[i+1][:1])
+
+    if new_list[i] == "down":
+        depthCount = depthCount - int(new_list[i+1][:1])
+
+
+print(forwardCount*depthCount)
 
 
 
