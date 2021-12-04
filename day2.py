590e6069e61bc9e8481cf9ee6e51d1f8d82b1821
MilesRay <89792182+MilesRay@users.noreply.github.com>
2021-12-04 20:29:14 +0000

    Day 2.2

--- day2.py
+++ day2.py
@@ -1,13 +1,3 @@
-f = open("day2.txt")
-
-file_contents = f.read()
-
-file_contents = file_contents.split(" ")
-new_list = [n for n in file_contents]
-
-for i in range(0, len(new_list)):
-    print(new_list[i])
-
 
 f = open("day2.txt")
 
@@ -15,22 +5,54 @@ file_contents = f.read()
 
 forwardCount = 0
 depthCount = 0
-file_contents = file_contents.split(" ")
+file_contents = file_contents.split("\n")
+
 new_list = [n for n in file_contents]
 
+
 for i in range(0, len(new_list)):
-    if new_list[i] == "forward":
-        forwardCount = forwardCount + int(new_list[i+1][:1])
+    new_list[i] = new_list[i].split(" ")
+    
+print(new_list)
+for i in range(0, len(new_list)):
+    if new_list[i][0] == "forward":
+        forwardCount = forwardCount + int(new_list[i][1])
 
-    if new_list[i] == "up":
-        depthCount = depthCount + int(new_list[i+1][:1])
+    if new_list[i][0] == "up":
+        depthCount = depthCount - int(new_list[i][1])
 
-    if new_list[i] == "down":
-        depthCount = depthCount - int(new_list[i+1][:1])
+    if new_list[i][0] == "down":
+        depthCount = depthCount + int(new_list[i][1])
 
 
 print(forwardCount*depthCount)
 
 
+#part 2
 
 
+file_contents = open("day2.txt").read()
+
+forwardCount = 0
+depthCount = 0
+file_contents = file_contents.split("\n")
+
+new_list = [n for n in file_contents]
+
+
+for i in range(0, len(new_list)):
+    new_list[i] = new_list[i].split(" ")
+    
+print(new_list)
+for i in range(0, len(new_list)):
+    if new_list[i][0] == "forward":
+        forwardCount = forwardCount + int(new_list[i][1])
+
+    if new_list[i][0] == "up":
+        depthCount = depthCount - int(new_list[i][1])
+
+    if new_list[i][0] == "down":
+        depthCount = depthCount + int(new_list[i][1])
+
+
+print(forwardCount*depthCount)
